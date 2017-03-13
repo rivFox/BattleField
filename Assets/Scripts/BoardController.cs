ad3bdf69a1e40dd5265931d5dbe3f065d66feca0
Bartłomiej Jarosz <rivfox.jb@gmail.com>
2017-03-13 23:30:58 +0100

    first commit

---
+++ Assets/Scripts/BoardController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardController : MonoBehaviour {
+
+    public GameObject playerLeft;
+    public GameObject playerRight;
+
+    public GameObject plane;
+
+    float margin = 0f;
+
+	// Use this for initialization
+	void Start () {
+        CreateMap(5, 10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void CreateMap(int _y, int _x)
+    {
+        for(int iz = 0; iz < _y; iz++)
+        {
+            for(int ix = 0; ix < _x; ix++)
+            {
+                Instantiate(plane, new Vector3(ix, 0f, iz + iz*margin), Quaternion.Euler(0f, 0f, 0f));
+            }
+        }
+        Instantiate(playerLeft, new Vector3(-1, 0f, 0f), Quaternion.Euler(0f, 0f, 0f));
+        Instantiate(playerRight, new Vector3(_x, 0f, 0f), Quaternion.Euler(0f, 0f, 0f));  
+    }
+    
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public bool playerRight = false;
+    public int position = 0;
+    public int maxPosition = 5;
+
+    public GameObject[] soldiers;
+
+    int pointerToSoldier = 0;
+    GameObject pointerToSoldierPrefab;
+
+    string str_player;
+    Quaternion soldierRotation;
+
+	// Use this for initialization
+	void Start () {
+        if(playerRight)
+        {
+            str_player = "P2";
+            soldierRotation = Quaternion.Euler(-90f, 0f, 0f);
+        }
+        else
+        {
+            str_player = "P1";
+            soldierRotation = Quaternion.Euler(-90f, 180f, 0f);
+        }
+
+        pointerToSoldierPrefab = Instantiate(soldiers[pointerToSoldier], transform.position, soldierRotation);
+        pointerToSoldierPrefab.transform.parent = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ChangePosition();
+        if (Input.GetButtonDown(str_player + "Back"))
+        {
+            ChangeSoldier();
+        }
+
+    }
+
+    void ChangePosition()
+    {
+        if (Input.GetButtonDown(str_player + "Up"))
+        {
+            if(position < maxPosition-1)
+            {
+                position++;
+            }
+        }
+        if (Input.GetButtonDown(str_player + "Down"))
+        {
+            if (position > 0)
+            {
+                position--;
+            }
+        }
+        transform.position = new Vector3(transform.position.x, 0f, position);
+    }
+
+    void ChangeSoldier()
+    {
+        if(pointerToSoldier < soldiers.Length - 1)
+        {
+            pointerToSoldier++;
+        }
+        else
+        {
+            pointerToSoldier = 0;
+        }
+
+        Destroy(pointerToSoldierPrefab);
+        pointerToSoldierPrefab = Instantiate(soldiers[pointerToSoldier], transform.position, soldierRotation);
+        pointerToSoldierPrefab.transform.parent = transform;
+
+    }
+}
